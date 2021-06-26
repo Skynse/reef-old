3e57c88200f85e35777e7bdb232f829a6bfba506
Skynse <prodbyskynse@gmail.com>
2021-06-26 17:03:43 -0500

    Change __main__.py structure

--- reef/__main__.py
+++ reef/__main__.py
@@ -1,18 +1,6 @@
 import argparse
-import scanner
+from reef.cli import main
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
+    main()
 
-    parser.add_argument("-sP", help="Scan for open ports in given ip")
-    parser.add_argument("-sI", help="Scan IP addresses in subnet", action="store_true")
-
-    args = parser.parse_args()
-
-    if args.sP:
-        print("Port Scan enabled")
-        scanner.PortScanner(args.sP).run()
-
-    if args.sI:
-        print("IP Scan enabled")
-        scanner.IPScanner().scan()
