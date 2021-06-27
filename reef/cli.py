87035289cc3bd49cc2464437f3e8f5e7215bf4ac
Skynse <prodbyskynse@gmail.com>
2021-06-27 03:06:22 -0500

    removed get_gateway() and replaced with utility

--- reef/cli.py
+++ reef/cli.py
@@ -1,12 +1,20 @@
 import argparse
 from reef import scanner
+from reef import arpspoof
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(prog="reef")
 
     parser.add_argument("-sP", help="Scan for open ports in given ip")
-    parser.add_argument("-sI", help="Scan IP addresses in subnet", action="store_true")
+
+    ipargs = parser.add_argument_group("Ip Scan")
+    ipargs.add_argument("-sI", help="Scan IP addresses in subnet", action="store_true")
+
+    arpargs = parser.add_argument_group("Arpspoofer")
+    arpargs.add_argument("-i", "--spoof", help="Spoofing IP")
+    arpargs.add_argument("-t", "--target", help="Target IP")
+    arpargs.add_argument("-g", "--gateway", help="Router")
 
     args = parser.parse_args()
 
@@ -17,3 +25,6 @@ def main():
     if args.sI:
         print("IP Scan enabled")
         scanner.IPScanner().scan()
+
+    if args.spoof:
+        arpspoof.ArpSpoofer(args.target, args.spoof, args.gateway).execute()
